3a1cb5f26d8fbcb663d896cc9dbdef18f3826ff8
Diego Coelho <diegocoelhoaprova@gmail.com>
2026-03-01 22:34:36 -0300

    Refactor Answer and Question entities to use props in constructors; update tests and add AnswersRepository interface

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer.js"
+import type { AnswersRepository } from "../repositories/answers-repository.js"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -7,8 +8,20 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+  constructor(
+    private answersRepository: AnswersRepository,
+  ) {
+
+  }
+
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      authorId: instructorId,
+      content,  
+      questionId,
+    })
+
+    await this.answersRepository.create(answer)
 
     return answer
   }
