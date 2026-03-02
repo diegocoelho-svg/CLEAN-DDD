2de19d032adf366a733674a6d05c0a3707ca90a0
Diego Coelho <diegocoelhoaprova@gmail.com>
2026-03-01 23:47:23 -0300

    Refactor domain entities to extend Entity class, removing redundant properties and constructors

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from "node:crypto";
 import type { SlugVO } from "./value-objects/slug.js";
+import { Entity } from "../../core/entities/entity.js";
 
 interface QuestionProps {
   title: string
@@ -7,18 +7,5 @@ interface QuestionProps {
   slug: SlugVO
   authorId: string
 }
-export class Question {
-  public id: string;
-  public title: string;
-  public slug: SlugVO
-  public content: string;
-  public authorId: string;
-
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title
-    this.content = props.content
-    this.slug = props.slug
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
+export class Question extends Entity<QuestionProps> {
 }
